8864a27c4ffe7b8d8df335da39e685aec7e525e1
Andrew Gieraltowski <37463248+andygten@users.noreply.github.com>
2023-12-04 23:01:46 -0500

    Working deletion of firestore messages

--- firestore.js
+++ firestore.js
@@ -6,6 +6,7 @@
 
 
 let messageList = null
+let currentUser = ""
 
 window.onload = function() {
     messageList = document.querySelector("#messageList");
@@ -28,9 +29,11 @@ window.onload = function() {
     form.message.value = "";
     });
 
+
     document.getElementById("requestContactData").addEventListener('submit', event=> {
         messageList.innerHTML = '';
         const email = document.getElementById("requestEmail").value;
+        currentUser = email
         event.preventDefault();
         db.collection("contactData").where("email", "==", email)
         .get()
@@ -45,8 +48,6 @@ window.onload = function() {
 
 function buildMessageList(doc) {
 
-    // Clear existing messages
-
     let li = document.createElement("li");
     let message = document.createElement("span");
     let cross = document.createElement("div");
@@ -56,15 +57,32 @@ function buildMessageList(doc) {
     cross.textContent = "x";
 
     li.appendChild(message);
+    li.appendChild(cross);
     messageList.appendChild(li);
 
     // deleting data
     cross.addEventListener("click", e => {
-      e.stopPropagation();
-      let id = e.target.parentElement.getAttribute("data-id");
-      db.collection("contactData")
-        .doc(id)
-        .delete();
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute("data-id");
+        db.collection("contactData")
+            .doc(id)
+            .delete();
+
+        
+        // Listen for real time updates and reflect list
+        db.collection("contactData").where("email", "==", currentUser).onSnapshot(snapshot => {
+            messageList.innerHTML = '';
+            let changes = snapshot.docChanges();
+            changes.forEach(change => {
+                console.log(change.doc.data());
+                if (change.type == "added") {
+                    buildMessageList(change.doc);
+                } else if (change.type == "removed") {
+                    let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
+                    cafeList.removeChild(li);
+                }
+            });
+        })
     });
 }
 
